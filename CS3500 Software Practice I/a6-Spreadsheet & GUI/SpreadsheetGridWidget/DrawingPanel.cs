a7beccebd897d4018fc3a1c1edf226014a05ab52
Jonathan Gage Buss <73267321+yew12@users.noreply.github.com>
2022-10-31 16:09:29 -0600

    Add files via upload

---
+++ CS3500 Software Practice I/a6-Spreadsheet & GUI/SpreadsheetGridWidget/DrawingPanel.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SpreadsheetGrid_Core
+{
+    /// <summary>
+    /// The panel where the spreadsheet grid is drawn.  It keeps track of the
+    /// current selection as well as what is supposed to be drawn in each cell.
+    /// </summary>
+    public partial class DrawingPanel : UserControl
+    {
+
+        // Columns and rows are numbered beginning with 0.  This is the coordinate
+        // of the selected cell.
+        private int _selectedCol;
+        private int _selectedRow;
+
+        // Coordinate of cell in upper-left corner of display
+        private int _firstColumn = 0;
+        private int _firstRow = 0;
+
+        // The strings contained by the spreadsheet
+        private Dictionary<SpreadsheetGridWidget.Address, String> _values;
+
+        // The containing panel
+        private SpreadsheetGridWidget _ssp;
+
+        public DrawingPanel() : this(null) { }
+
+        public DrawingPanel(SpreadsheetGridWidget ss = null)
+        {
+            InitializeComponent();
+
+            DoubleBuffered = true;
+            _values = new Dictionary<SpreadsheetGridWidget.Address, String>();
+            _ssp = ss;
+        }
+
+        public void SetGrid(SpreadsheetGridWidget ss)
+        {
+            _ssp = ss;
+        }
+
+        private bool InvalidAddress(int col, int row)
+        {
+            return col < 0 || row < 0 || col >= SpreadsheetGridWidget.COL_COUNT || row >= SpreadsheetGridWidget.ROW_COUNT;
+        }
+
+        public void Clear()
+        {
+            _values.Clear();
+            Invalidate();
+        }
+
+        public bool SetValue(int col, int row, string c)
+        {
+            if (InvalidAddress(col, row))
+            {
+                return false;
+            }
+
+            SpreadsheetGridWidget.Address a = new SpreadsheetGridWidget.Address(col, row);
+            if (c == null || c == "")
+            {
+                _values.Remove(a);
+            }
+            else
+            {
+                _values[a] = c;
+            }
+            Invalidate();
+            return true;
+        }
+
+        public bool GetValue(int col, int row, out string c)
+        {
+            if (InvalidAddress(col, row))
+            {
+                c = null;
+                return false;
+            }
+            if (!_values.TryGetValue(new SpreadsheetGridWidget.Address(col, row), out c))
+            {
+                c = "";
+            }
+            return true;
+        }
+
+        public bool SetSelection(int col, int row)
+        {
+            if (InvalidAddress(col, row))
+            {
+                return false;
+            }
+            _selectedCol = col;
+            _selectedRow = row;
+            Invalidate();
+            return true;
+        }
+
+        public void GetSelection(out int col, out int row)
+        {
+            col = _selectedCol;
+            row = _selectedRow;
+        }
+
+        public void HandleHScroll(Object sender, ScrollEventArgs args)
+        {
+            Console.WriteLine(args.NewValue);
+            _firstColumn = args.NewValue;
+            Invalidate();
+        }
+
+        public void HandleVScroll(Object sender, ScrollEventArgs args)
+        {
+            _firstRow = args.NewValue;
+            Invalidate();
+        }
+
+        /// <summary>
+        /// This is the "heart" of the program. It generates the look and feel of multiple textboxes, but
+        /// does so by just drawing lines and text on the screen!
+        /// </summary>
+        /// <param name="e"> The "Graphics" object used to paint the contents on the screen </param>
+        protected override void OnPaint(PaintEventArgs e)
+        {
+
+            // Clip based on what needs to be refreshed.
+            Region clip = new Region(e.ClipRectangle);
+            e.Graphics.Clip = clip;
+
+            // Color the background of the data area white
+            e.Graphics.FillRectangle(
+                new SolidBrush(Color.White),
+                SpreadsheetGridWidget.LABEL_COL_WIDTH,
+                SpreadsheetGridWidget.LABEL_ROW_HEIGHT,
+                (SpreadsheetGridWidget.COL_COUNT - _firstColumn) * SpreadsheetGridWidget.DATA_COL_WIDTH,
+                (SpreadsheetGridWidget.ROW_COUNT - _firstRow) * SpreadsheetGridWidget.DATA_ROW_HEIGHT);
+
+            // Pen, brush, and fonts to use
+            Brush brush = new SolidBrush(Color.Black);
+            Pen pen = new Pen(brush);
+            Font regularFont = Font;
+            Font boldFont = new Font(regularFont, FontStyle.Bold);
+
+            // Draw the column lines
+            int bottom = SpreadsheetGridWidget.LABEL_ROW_HEIGHT + (SpreadsheetGridWidget.ROW_COUNT - _firstRow) * SpreadsheetGridWidget.DATA_ROW_HEIGHT;
+            e.Graphics.DrawLine(pen, new Point(0, 0), new Point(0, bottom));
+            for (int x = 0; x <= (SpreadsheetGridWidget.COL_COUNT - _firstColumn); x++)
+            {
+                e.Graphics.DrawLine(
+                    pen,
+                    new Point(SpreadsheetGridWidget.LABEL_COL_WIDTH + x * SpreadsheetGridWidget.DATA_COL_WIDTH, 0),
+                    new Point(SpreadsheetGridWidget.LABEL_COL_WIDTH + x * SpreadsheetGridWidget.DATA_COL_WIDTH, bottom));
+            }
+
+            // Draw the column labels
+            for (int x = 0; x < SpreadsheetGridWidget.COL_COUNT - _firstColumn; x++)
+            {
+                Font f = (_selectedCol - _firstColumn == x) ? boldFont : Font;
+                DrawColumnLabel(e.Graphics, x, f);
+            }
+
+            // Draw the row lines
+            int right = SpreadsheetGridWidget.LABEL_COL_WIDTH + (SpreadsheetGridWidget.COL_COUNT - _firstColumn) * SpreadsheetGridWidget.DATA_COL_WIDTH;
+            e.Graphics.DrawLine(pen, new Point(0, 0), new Point(right, 0));
+            for (int y = 0; y <= SpreadsheetGridWidget.ROW_COUNT - _firstRow; y++)
+            {
+                e.Graphics.DrawLine(
+                    pen,
+                    new Point(0, SpreadsheetGridWidget.LABEL_ROW_HEIGHT + y * SpreadsheetGridWidget.DATA_ROW_HEIGHT),
+                    new Point(right, SpreadsheetGridWidget.LABEL_ROW_HEIGHT + y * SpreadsheetGridWidget.DATA_ROW_HEIGHT));
+            }
+
+            // Draw the row labels
+            for (int y = 0; y < (SpreadsheetGridWidget.ROW_COUNT - _firstRow); y++)
+            {
+                Font f = (_selectedRow - _firstRow == y) ? boldFont : Font;
+                DrawRowLabel(e.Graphics, y, f);
+            }
+
+            // Highlight the selection, if it is visible
+            if ((_selectedCol - _firstColumn >= 0) && (_selectedRow - _firstRow >= 0))
+            {
+                e.Graphics.DrawRectangle(
+                    pen,
+                    new Rectangle(SpreadsheetGridWidget.LABEL_COL_WIDTH + (_selectedCol - _firstColumn) * SpreadsheetGridWidget.DATA_COL_WIDTH + 1,
+                                  SpreadsheetGridWidget.LABEL_ROW_HEIGHT + (_selectedRow - _firstRow) * SpreadsheetGridWidget.DATA_ROW_HEIGHT + 1,
+                                  SpreadsheetGridWidget.DATA_COL_WIDTH - 2,
+                                  SpreadsheetGridWidget.DATA_ROW_HEIGHT - 2));
+            }
+
+            // Draw the text
+            foreach (KeyValuePair<SpreadsheetGridWidget.Address, String> address in _values)
+            {
+                String text = address.Value;
+                int x = address.Key.Col - _firstColumn;
+                int y = address.Key.Row - _firstRow;
+                float height = e.Graphics.MeasureString(text, regularFont).Height;
+                float width = e.Graphics.MeasureString(text, regularFont).Width;
+                if (x >= 0 && y >= 0)
+                {
+                    Region cellClip = new Region(new Rectangle(SpreadsheetGridWidget.LABEL_COL_WIDTH + x * SpreadsheetGridWidget.DATA_COL_WIDTH + SpreadsheetGridWidget.PADDING,
+                                                               SpreadsheetGridWidget.LABEL_ROW_HEIGHT + y * SpreadsheetGridWidget.DATA_ROW_HEIGHT,
+                                                               SpreadsheetGridWidget.DATA_COL_WIDTH - 2 * SpreadsheetGridWidget.PADDING,
+                                                               SpreadsheetGridWidget.DATA_ROW_HEIGHT));
+                    cellClip.Intersect(clip);
+                    e.Graphics.Clip = cellClip;
+                    e.Graphics.DrawString(
+                        text,
+                        regularFont,
+                        brush,
+                        SpreadsheetGridWidget.LABEL_COL_WIDTH + x * SpreadsheetGridWidget.DATA_COL_WIDTH + SpreadsheetGridWidget.PADDING,
+                        SpreadsheetGridWidget.LABEL_ROW_HEIGHT + y * SpreadsheetGridWidget.DATA_ROW_HEIGHT + (SpreadsheetGridWidget.DATA_ROW_HEIGHT - height) / 2);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Draws a column label.  The columns are indexed beginning with zero.
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="x"></param>
+        /// <param name="f"></param>
+        private void DrawColumnLabel(Graphics g, int x, Font f)
+        {
+            String label = ((char)('A' + x + _firstColumn)).ToString();
+            float height = g.MeasureString(label, f).Height;
+            float width = g.MeasureString(label, f).Width;
+            g.DrawString(
+                  label,
+                  f,
+                  new SolidBrush(Color.Black),
+                  SpreadsheetGridWidget.LABEL_COL_WIDTH + x * SpreadsheetGridWidget.DATA_COL_WIDTH + (SpreadsheetGridWidget.DATA_COL_WIDTH - width) / 2,
+                  (SpreadsheetGridWidget.LABEL_ROW_HEIGHT - height) / 2);
+        }
+
+        /// <summary>
+        /// Draws a row label.  The rows are indexed beginning with zero.
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="y"></param>
+        /// <param name="f"></param>
+        private void DrawRowLabel(Graphics g, int y, Font f)
+        {
+            String label = (y + 1 + _firstRow).ToString();
+            float height = g.MeasureString(label, f).Height;
+            float width = g.MeasureString(label, f).Width;
+            g.DrawString(
+                label,
+                f,
+                new SolidBrush(Color.Black),
+                SpreadsheetGridWidget.LABEL_COL_WIDTH - width - SpreadsheetGridWidget.PADDING,
+                SpreadsheetGridWidget.LABEL_ROW_HEIGHT + y * SpreadsheetGridWidget.DATA_ROW_HEIGHT + (SpreadsheetGridWidget.DATA_ROW_HEIGHT - height) / 2);
+        }
+
+        /// <summary>
+        /// Determines which cell, if any, was clicked.  Generates a SelectionChanged event.  All of
+        /// the indexes are zero based.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            base.OnClick(e);
+            int x = (e.X - SpreadsheetGridWidget.LABEL_COL_WIDTH) / SpreadsheetGridWidget.DATA_COL_WIDTH;
+            int y = (e.Y - SpreadsheetGridWidget.LABEL_ROW_HEIGHT) / SpreadsheetGridWidget.DATA_ROW_HEIGHT;
+            if (e.X > SpreadsheetGridWidget.LABEL_COL_WIDTH && e.Y > SpreadsheetGridWidget.LABEL_ROW_HEIGHT && (x + _firstColumn < SpreadsheetGridWidget.COL_COUNT) && (y + _firstRow < SpreadsheetGridWidget.ROW_COUNT))
+            {
+                _selectedCol = x + _firstColumn;
+                _selectedRow = y + _firstRow;
+                if (_ssp != null)
+                {
+                    _ssp.SetSelection(_selectedCol, _selectedRow);
+                }
+                //if (_ssp.SelectionChanged != null)
+                //{
+                //    _ssp.SelectionChanged(_ssp);
+                //} 
+            }
+            Invalidate();
+        }
+
+        private void DrawingPanel_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ CS3500 Software Practice I/a6-Spreadsheet & GUI/SpreadsheetGridWidget/SpreadsheetGridWidget.cs
@@ -0,0 +1,238 @@
+﻿/// <summary>
+///   Author(s):       Joe Zachary
+///                    Jim de St. Germain - Refactor/Documentation for ASP Core
+///   Course:          CS 3500
+///   Date:            2011 Sept   - Initial Grid Code
+///                    2020 Spring - Refactor for ASP Core
+///
+///   
+///   Partial:
+/// 
+///     The code for this class is divided into two files using C#'s partial ability.
+///     The other half is in the SpreadsheetGridWidget.Designer.cs file.  The Designer
+///     code is generally created automatically by the WYSIWYG editor.
+/// 
+///   Contents:
+///   
+///   This class represents a GRID that looks like (behaves as) a spreadsheet interface.
+///   
+///   This could have been done by creating a giant 2D array of textbox widgets, but the
+///   overhead and inefficiency is too great.  Therefore, a "drawing panel" which simply
+///   draws lines and text such that it looks like a bunch of textboxes is used.
+///   
+///   This code determines the location (based on scrolling) for the upper left cell
+///   and then draws a "picture" for the displayable "image" of the spreadsheet.
+/// 
+/// </summary>
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace SpreadsheetGrid_Core
+{
+    /// <summary>
+    /// The type of delegate used to register for SelectionChanged events
+    /// </summary>
+    /// <param name="sender"></param>
+    public delegate void SelectionChangedHandler(SpreadsheetGridWidget sender);
+
+    /// <summary>
+    /// A panel that displays a spreadsheet with 26 columns (labeled A-Z) and 99 rows
+    /// (labeled 1-99).  Each cell on the grid can display a non-editable string.  One 
+    /// of the cells is always selected (and highlighted).  When the selection changes, a 
+    /// SelectionChanged event is fired.  Clients can register to be notified of
+    /// such events.
+    /// 
+    /// None of the cells are editable.  They are for display purposes only.
+    /// </summary>
+    public partial class SpreadsheetGridWidget : UserControl
+    {
+        /// <summary>
+        /// The event used to send notifications of a selection change
+        /// </summary>
+        public event SelectionChangedHandler SelectionChanged;
+
+        // These constants control the layout of the spreadsheet grid.  The height and
+        // width measurements are in pixels.
+        internal const int DATA_COL_WIDTH = 80;
+        internal const int DATA_ROW_HEIGHT = 20;
+        internal const int LABEL_COL_WIDTH = 30;
+        internal const int LABEL_ROW_HEIGHT = 30;
+        internal const int PADDING = 2;
+        internal const int SCROLLBAR_WIDTH = 20;
+        internal const int COL_COUNT = 26;
+        internal const int ROW_COUNT = 99;
+
+
+        /// <summary>
+        /// Creates the Spreadsheet Grid Widget.  In this case, an empty one
+        /// with no values.  Code similar to this can be created via the GUI WYSIWYG designer
+        /// but in this case, the code was manually constructed.
+        /// 
+        /// The InitializeComponent calls the auto-generated code
+        /// </summary>
+
+        public SpreadsheetGridWidget()
+        {
+            InitializeComponent();
+
+            // after the base initialization, we have to do some tweaks
+            // that are not available from the WYSIWYG designer
+
+            this.drawingPanel.SetGrid(this); // associate the grid panel with "this" 
+            this.hScroll.Scroll += drawingPanel.HandleHScroll;
+            this.vScroll.Scroll += drawingPanel.HandleVScroll;
+
+            this.hScroll.Maximum = COL_COUNT;
+            this.hScroll.SmallChange = 1;
+            this.vScroll.Maximum = ROW_COUNT + 1;
+            this.vScroll.SmallChange = 1;
+        }
+
+
+        /// <summary>
+        /// Clears the display.
+        /// </summary>
+
+        public void Clear()
+        {
+            drawingPanel.Clear();
+        }
+
+
+        /// <summary>
+        /// If the zero-based column and row are in range, sets the value of that
+        /// cell and returns true.  Otherwise, returns false.
+        /// </summary>
+        /// <param name="col"></param>
+        /// <param name="row"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool SetValue(int col, int row, string value)
+        {
+            return drawingPanel.SetValue(col, row, value);
+        }
+
+
+        /// <summary>
+        /// If the zero-based column and row are in range, assigns the value
+        /// of that cell to the out parameter and returns true.  Otherwise,
+        /// returns false.
+        /// </summary>
+        /// <param name="col"></param>
+        /// <param name="row"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool GetValue(int col, int row, out string value)
+        {
+            return drawingPanel.GetValue(col, row, out value);
+        }
+
+
+        /// <summary>
+        /// If the zero-based column and row are in range, uses them to set
+        /// the current selection and returns true.  Otherwise, returns false.
+        /// </summary>
+        /// <param name="col"></param>
+        /// <param name="row"></param>
+        /// <param name="changed"> whether to call the event handler </param>
+        /// <returns></returns>
+
+        public bool SetSelection(int col, int row, bool changed = true)
+        {
+            var temp = drawingPanel.SetSelection(col, row);
+
+            if (changed)
+            {
+                SelectionChanged(this);
+            }
+            return temp;
+        }
+
+
+        /// <summary>
+        /// Assigns the column and row of the current selection to the
+        /// out parameters.
+        /// </summary>
+        /// <param name="col"></param>
+        /// <param name="row"></param>
+
+        public void GetSelection(out int col, out int row)
+        {
+            drawingPanel.GetSelection(out col, out row);
+        }
+
+
+        /// <summary>
+        /// When the SpreadsheetPanel is resized, we set the size and locations of the three
+        /// components that make it up.
+        /// </summary>
+        /// <param name="eventargs"></param>
+
+        protected override void OnResize(EventArgs eventargs)
+        {
+            base.OnResize(eventargs);
+            try
+            {
+                if (FindForm() == null || FindForm().WindowState != FormWindowState.Minimized)
+                {
+                    if (Height < 1)
+                    {
+                        drawingPanel.Size = new Size(100, 100);
+                        return;
+                    }
+                    drawingPanel.Size = new Size(Width - SCROLLBAR_WIDTH, Height - SCROLLBAR_WIDTH);
+                    vScroll.Location = new Point(Width - SCROLLBAR_WIDTH, 0);
+                    vScroll.Size = new Size(SCROLLBAR_WIDTH, Height - SCROLLBAR_WIDTH);
+                    vScroll.LargeChange = (Height - SCROLLBAR_WIDTH) / DATA_ROW_HEIGHT;
+                    hScroll.Location = new Point(0, Height - SCROLLBAR_WIDTH);
+                    hScroll.Size = new Size(Width - SCROLLBAR_WIDTH, SCROLLBAR_WIDTH);
+                    hScroll.LargeChange = (Width - SCROLLBAR_WIDTH) / DATA_COL_WIDTH;
+                }
+            }
+            catch (Exception)
+            {
+                ; // no action taken 
+            }
+        }
+
+
+
+        /// <summary>
+        /// Used internally to keep track of cell addresses
+        /// </summary>
+        internal class Address
+        {
+
+            public int Col { get; set; }
+            public int Row { get; set; }
+
+            public Address(int c, int r)
+            {
+                Col = c;
+                Row = r;
+            }
+
+            public override int GetHashCode()
+            {
+                return Col.GetHashCode() ^ Row.GetHashCode();
+            }
+
+            public override bool Equals(object obj)
+            {
+                if ((obj == null) || !(obj is Address))
+                {
+                    return false;
+                }
+                Address a = (Address)obj;
+                return Col == a.Col && Row == a.Row;
+            }
+        }
+
+       
+    }
+
+  
+}
